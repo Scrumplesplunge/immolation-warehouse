fb8fc04f0f4018d65111faacb0e853ab74e4bbfc
Elliot Prior <elliotprior@gmail.com>
2014-02-22 18:38:32 +0000

    BIGWINDOW

--- ImmolationWarehouse-desktop/src/net/compsoc/ox/iw/Main.java
+++ ImmolationWarehouse-desktop/src/net/compsoc/ox/iw/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Immolation Warehouse";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 1920;
+		cfg.height = 1280;
 		
 		new LwjglApplication(new MainGame(), cfg);
 	}
