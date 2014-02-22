a82f138e96b1bdda278ab5439f156d01fe65d4d8
James Wallis <shroom.monk@googlemail.com>
2014-02-22 19:05:41 +0000

    Tidied up texture memory management.

--- ImmolationWarehouse/src/net/compsoc/ox/iw/MainGame.java
+++ ImmolationWarehouse/src/net/compsoc/ox/iw/MainGame.java
@@ -4,19 +4,13 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-//import com.badlogic.gdx.graphics.Texture;
-//import com.badlogic.gdx.graphics.Texture.TextureFilter;
-//import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-//import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import net.compsoc.ox.iw.common.MusicManager;
 import aurelienribon.tweenengine.TweenManager;
 
 public class MainGame implements ApplicationListener {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	//private Texture texture;
-	//private Sprite sprite;
 	public static MusicManager music = new MusicManager();
 	public static TweenManager tweenManager = new TweenManager();
 	
@@ -24,32 +18,19 @@ public class MainGame implements ApplicationListener {
 	
 	@Override
 	public void create() {
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+		//float w = Gdx.graphics.getWidth();
+		//float h = Gdx.graphics.getHeight();
 		
 		camera = new OrthographicCamera(512, 512);
 		batch = new SpriteBatch();
 		
 		demoLevel = new Level();
-		
-		/*
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		//TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(texture);
-		sprite.setSize(1.0f, 1.0f);
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
-		System.out.println(sprite.getWidth());
-		*/
 	}
 
 	@Override
 	public void dispose() {
+		demoLevel.dispose();
 		batch.dispose();
-		//texture.dispose();
 	}
 
 	@Override
@@ -59,7 +40,6 @@ public class MainGame implements ApplicationListener {
 		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		//sprite.draw(batch);
 		demoLevel.render(batch);
 		batch.end();
 	}
