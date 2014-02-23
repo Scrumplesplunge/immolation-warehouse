cacb71d894de8ae695df7387129427aa3bdedfb0
James Wallis <shroom.monk@googlemail.com>
2014-02-23 00:11:05 +0000

    Shitty 2D Vector class!

---
+++ ImmolationWarehouse/src/net/compsoc/ox/iw/Vector2.java
@@ -0,0 +1,16 @@
+package net.compsoc.ox.iw;
+
+/*
+ * Datatype representing a 2D vector of type T
+ * By James
+ */
+public class Vector2<T> {
+	// Member variables
+	public T x, y;
+	
+	// Constructor
+	public Vector2(T x, T y) {
+		this.x = x;
+		this.y = y;
+	}
+}
